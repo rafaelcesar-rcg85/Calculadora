4ede39806b32bf3bbede71267836a679ff30db1f
Rafael Cesar Gonçalves <rafael.cesar@aluno.impacta.edu.br>
2026-03-27 09:00:26 -0300

    Segunda versao da calculadora

--- minha_calculadora.py
+++ minha_calculadora.py
@@ -1,10 +1,15 @@
 def soma(a, b):
     return a + b
 
+def subtracao(a, b):
+    return a - b
 def main():
     x = int(input("x? "))
     y = int(input("y? "))
     s = soma(x, y)
     print(f'Soma: {s}')
+    
+    t = subtracao(x, y)
+    print(f'Subtração: {t}')
     
 main()
